cbb8f2650ed2d59987b7d7fea4eeb973511af2cc
nacholupi <nacholupi@gmail.com>
2021-05-23 20:53:30 -0300

    Roll with validation

--- internal/app/game.go
+++ internal/app/game.go
@@ -1,9 +1,18 @@
 package app
 
+import "fmt"
+
 type Game struct {
 	score int
 }
 
 func (g Game) Score() int {
 	return g.score
+}
+
+func (g Game) Roll(pins int) error {
+	if pins > 10 || pins < 0 {
+		return fmt.Errorf("illegal argument: choose a number between 0 and 10")
+	}
+	return nil
 }
--- internal/app/game_test.go
+++ internal/app/game_test.go
@@ -5,13 +5,34 @@ import (
 	"testing"
 )
 
-func TestGame(t *testing.T) {
-	assert.Equal(t, true, true)
-}
-
-func TestInitScoreZero(t *testing.T) {
+func TestGame_Score_InitZero(t *testing.T) {
 	g := Game{}
+
 	score := g.Score()
 
 	assert.Equal(t, 0, score)
+}
+
+func TestGame_Roll(t *testing.T) {
+	g := Game{}
+
+	err := g.Roll(10)
+
+	assert.NoError(t, err)
+}
+
+func TestGame_RollMoreThanTen(t *testing.T) {
+	g := Game{}
+
+	err := g.Roll(11)
+
+	assert.Error(t, err)
+}
+
+func TestGame_RollLessThanZero(t *testing.T) {
+	g := Game{}
+
+	err := g.Roll(-1)
+
+	assert.Error(t, err)
 }
