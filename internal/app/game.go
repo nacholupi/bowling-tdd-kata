909a719ebb287416ffb920ac30f377ae2cd9f014
nacholupi <nacholupi@gmail.com>
2021-05-23 21:09:24 -0300

    Retain score

--- internal/app/game.go
+++ internal/app/game.go
@@ -10,9 +10,11 @@ func (g Game) Score() int {
 	return g.score
 }
 
-func (g Game) Roll(pins int) error {
+func (g *Game) Roll(pins int) error {
 	if pins > 10 || pins < 0 {
 		return fmt.Errorf("illegal argument: choose a number between 0 and 10")
 	}
+
+	g.score = pins
 	return nil
 }
--- internal/app/game_test.go
+++ internal/app/game_test.go
@@ -21,18 +21,37 @@ func TestGame_Roll(t *testing.T) {
 	assert.NoError(t, err)
 }
 
-func TestGame_RollMoreThanTen(t *testing.T) {
+func TestGame_Roll_returns_illegal_arg(t *testing.T) {
+	tests := []struct {
+		name string
+		pins int
+	}{
+		{
+			name: "LESS_THAN_ZERO",
+			pins: -1,
+		},
+		{
+			name: "MORE_THAN_TEN",
+			pins: 11,
+		},
+	}
+
 	g := Game{}
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
 
-	err := g.Roll(11)
+			err := g.Roll(test.pins)
 
-	assert.Error(t, err)
+			assert.Error(t, err)
+		})
+	}
 }
 
-func TestGame_RollLessThanZero(t *testing.T) {
+func TestGame_Roll_retain_score(t *testing.T) {
 	g := Game{}
 
-	err := g.Roll(-1)
+	err := g.Roll(10)
 
-	assert.Error(t, err)
+	assert.NoError(t, err)
+	assert.Equal(t, 10, g.Score())
 }
